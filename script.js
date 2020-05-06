626e42226faa4aa8f4d1e9e69ab44dfc3eef99e6
Tobipeters <oluwatobilobapeterschem@gmail.com>
2020-05-06 17:34:40 +0100

    updated functions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let taskHolder = [];
-let getTask = [];
 let day = document.getElementById('day');
 let dayNumber = document.getElementById('day-no');
 let monthAndYear = document.getElementById('month-and-year');
@@ -9,56 +8,54 @@ let months = ["January", "February", "March", "April", "May", "June", "July", "A
 let month = months[date.getMonth()]
 let year = date.getFullYear();
 
+let getTime = document.getElementById('get-time');
+let getTitle = document.getElementById('get-title');
+let list = document.getElementById('list');
 
 dayNumber.innerHTML = date.getDate();
 day.innerHTML = days[date.getDay()];
 monthAndYear.innerHTML = `${month} ${year}`
 
-//Function for adding task to localStorage
+//load initial storage items
+// initialLoad = () =>{
+//     if (taskHolder.length <= 0 && localStorage.task ){
+//         console.log(localStorage.task);
+//         taskHolder.JSON.parse(localStorage.task);
+//         const items = taskHolder.map((elem)=>{
+//             list.append(...items);
+//         })
+//     }
+//     displayTask(items)
+// }
 
-addTask = () => {
+// initialLoad();
+addTask = () =>{
     let taskName = document.getElementById('task').value;
     let taskTime = document.getElementById('time').value;
-    let getTime = document.getElementById('get-time');
-    let getTitle = document.getElementById('get-title');
-
-    for (let i = 0; i < taskHolder.length; i++) {
-        const element = taskHolder[i];
-        console.log(element)
-    }
     const listOfTask = {
         task: taskName,
         time: taskTime
     }
-    taskHolder.push(listOfTask)
-    localStorage.setItem('tasks', JSON.stringify(taskHolder));
-    
-
+    taskHolder.push(listOfTask);
+    localStorage.task = JSON.stringify(taskHolder);
+    displayTask();
 }
-//function to get task from loclStorage
-allTasks = () => {
-    let getTime = document.getElementById('get-time');
-    let getTitle = document.getElementById('get-title');
-    let list = document.getElementById('list');
-    getTask = JSON.parse(localStorage.getItem('tasks'));
-    let allTask = JSON.parse(localStorage.getItem('tasks'));
 
+displayTask = () =>{
+    let task = []
+    allTasks = JSON.parse(localStorage.task)
+   console.log(allTasks)
+}
+displayTask();
 
+deletTask =>{
 
-    // getTask.push(allTask)
-    // console.log(getTask);
-    // for (let i = 0; i < getTask.length; i++) {
-    //     console.log(getTask[i])
-    //     list.innerHTML = getTask[i].time;
-    //     list.innerHTML = getTask[i].task;
-    // }
 }
 
 
 toggle = () => {
     let dialog = document.getElementById('dialog_box');
     dialog.showModal();
-
 }
 /////////closing modal function
 closeModal = () => {
